7d3f88dcec6735b244de9999744eb3e16ce4ecd9
Dariusz Homa <45432804+Dar3cz3Q@users.noreply.github.com>
2024-05-08 17:51:35 +0200

    Added ternary operator in cors origin

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "" + process.env.APP_PROTOCOL + "://" + process.env.APP_IP + "",
+        origin: "" + process.env.APP_PROTOCOL + "://" + process.env.APP_IP + (process.env.NODE_ENV === "development" ? ":" + process.env.APP_PORT + "" : ""),
         optionsSuccessStatus: 200,
         methods: ["GET"],
         credentials: true,
